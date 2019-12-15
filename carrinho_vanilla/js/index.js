de76f3269b0df5f2d5f47dc70678866f02653699
Alex <Talha>
2019-12-15 16:10:34 -0300

    Dor da reatividade:

--- carrinho_vanilla/js/index.js
+++ carrinho_vanilla/js/index.js
@@ -1,35 +1,41 @@
 let products = [
 { 
+    "id" : "0",
     "imagem" : "https://lorempixel.com/500/300",
     "titulo" :  "Js Raiz 1",
     "preço" : 300,
     "descrição" : "O melhor curso de Javascript 1",
 },
 { 
+    "id" : "1",
     "imagem" : "https://lorempixel.com/500/300",
     "titulo" :  "Js Raiz 2",
     "preço" : 301,
     "descrição" : "O melhor curso de Javascript 2",
 },
 { 
+    "id" : "2",
     "imagem" : "https://lorempixel.com/500/300",
     "titulo" :  "Js Raiz 3",
     "preço" : 302,
     "descrição" : "O melhor curso de Javascript 4",
 },
 { 
+    "id" : "3",
     "imagem" : "https://lorempixel.com/500/300",
     "titulo" :  "Js Raiz 4",
     "preço" : 303,
     "descrição" : "O melhor curso de Javascript 4",
 },
 { 
+    "id" : "4",
     "imagem" : "https://lorempixel.com/500/300",
     "titulo" :  "Js Raiz 5",
     "preço" : 304,
     "descrição" : "O melhor curso de Javascript 5",
 },
 { 
+    "id" : "5",
     "imagem" : "https://lorempixel.com/500/300",
     "titulo" :  "Js Raiz 6",
     "preço" : 305,
@@ -38,39 +44,21 @@ let products = [
 ]
 
 
-let carrinhoItens = {
-
-    "bbc123" : {
-        id : "bbc123",
-        nome : "js raiz para node",
-        preco : 1200,
-        descricao : " O melhor curso de todos",
-        imagem : "https://lorempixel.com/500/300"
-
-    },
-    "cbc123" : {
-        id : "bbc123",
-        nome : "Programação funcional com JS",
-        preco : 500,
-        descricao : " O melhor funcional de todos",
-        imagem : "https://lorempixel.com/500/300"
-
-    },
-}
+let carrinhoItens = {}
 
 
 function renderProducts(products) {
     let arr = "";
     for(let i = 0; i < products.length; i++  ){
-       arr = arr +  renderProduct(products[i])
+       arr = arr +  renderProduct(products[i],i)
     }
 
- let loja =  document.querySelector('.loja')
- loja.innerHTML = arr;
+    let loja =  document.querySelector('.loja')
+    loja.innerHTML = arr;
 }
 
 
-function renderProduct(product){
+function renderProduct(product,index){
 
     return `
     <div class="col-sm-4 mb-3">
@@ -81,7 +69,7 @@ function renderProduct(product){
                     <h5 class="card-title">${product.titulo}</h5>
                     <small>R$ ${product.preço}</small>
                     <p class="card-text">${product.descrição}</p>
-                    <button class="btn btn-primary btn-add">Adicionar </button>
+                    <button data-index="${index}" class="btn btn-primary btn-add">Adicionar </button>
                 </div>
             </div>
         </div>
@@ -95,10 +83,10 @@ function renderItemCarrinho(itemCarrinho){
         return `
         <div class="card carrinho__item">
             <div class="card-body">
-                <h5 class="card-title">${itemCarrinho.nome}</h5>
-                <p class="card-text">Preço unidade : R$ ${itemCarrinho.preco} | Quantidade: 1 </p>
-                <p class="card-text">${itemCarrinho.descricao}</p>
-                <button data-value="300" class="btn btn-danger btn-sm">Remover </button>
+                <h5 class="card-title">${itemCarrinho.titulo}</h5>
+                <p class="card-text">Preço unidade : R$ ${itemCarrinho.preço} | Quantidade: ${itemCarrinho.quantidade} </p>
+                <p class="card-text">${itemCarrinho.descrição}</p>
+                <button data-value="${itemCarrinho.preço} " class="btn btn-danger btn-sm">Remover </button>
             </div>
         </div>
         `
@@ -114,14 +102,29 @@ function renderCarrinho(carrinhoItens){
 
 }
 
-
 document.body.addEventListener("click", function(event){
     const elemento = event.target
+    if(elemento.classList.contains('btn-add')){
+        const index = parseInt(elemento.getAttribute('data-index'))
+        const produto = products[index];
+        
 
-    console.log(elemento.classList.contains('btn-add'))
+        if(!carrinhoItens[produto.id]){
+            carrinhoItens[produto.id] = produto
+            carrinhoItens[produto.id].quantidade = 1
+
+            renderCarrinho(carrinhoItens);
+
+        }
+        
+    }
+
+
 })
 
+
+
 renderProducts(products)
-renderCarrinho(carrinhoItens);
+
 
 
