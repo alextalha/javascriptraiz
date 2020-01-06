c68cb5cfb5a704c98f318a2e68fccee54f5dfd71
Alex <alex.talha@original.io>
2020-01-06 18:56:25 -0300

    modularizando a parte de produto

--- carrinho_vanilla/js/react.js
+++ carrinho_vanilla/js/react.js
@@ -1,43 +1,87 @@
 
+let produtosListas = [
+    { 
+        "id" : "0",
+        "imagem" : "https://lorempixel.com/500/300",
+        "titulo" :  "Js Raiz 1",
+        "preço" : 300,
+        "descrição" : "O melhor curso de Javascript 1",
+    },
+    { 
+        "id" : "1",
+        "imagem" : "https://lorempixel.com/500/300",
+        "titulo" :  "Js Raiz 2",
+        "preço" : 301,
+        "descrição" : "O melhor curso de Javascript 2",
+    },
+    { 
+        "id" : "2",
+        "imagem" : "https://lorempixel.com/500/300",
+        "titulo" :  "Js Raiz 3",
+        "preço" : 302,
+        "descrição" : "O melhor curso de Javascript 4",
+    },
+    { 
+        "id" : "3",
+        "imagem" : "https://lorempixel.com/500/300",
+        "titulo" :  "Js Raiz 4",
+        "preço" : 303,
+        "descrição" : "O melhor curso de Javascript 4",
+    },
+    { 
+        "id" : "4",
+        "imagem" : "https://lorempixel.com/500/300",
+        "titulo" :  "Js Raiz 5",
+        "preço" : 304,
+        "descrição" : "O melhor curso de Javascript 5",
+    },
+    { 
+        "id" : "5",
+        "imagem" : "https://lorempixel.com/500/300",
+        "titulo" :  "Js Raiz 6",
+        "preço" : 305,
+        "descrição" : "O melhor curso de Javascript 6",
+    },
+    ]
+    
+
+function ProductComponent(produto){
+
+    return (
+
+        React.createElement('div',{className : "col-sm-4 mb-3" },
+            React.createElement('div',{className : "card loja__item " },
+                React.createElement('img',{className : "card-img-top", src: `${produto.imagem}` }),
+            React.createElement('div',{className : "card-body" },
+                React.createElement('h5',{className : "card-title" },`${produto.titulo}` ),
+                React.createElement('small',null,`${produto.preço}`),
+                React.createElement('p',{className : "card-text" },`${produto.descrição}`),
+                React.createElement('button',{className : "btn btn-primary" },'Adicionar'),
+                )
+            )
+        )
+    )
+}
+
+function ListaProdutoComponent({itens}){
+    
+    console.log(itens)
+    return  (
+        React.createElement('div',{className : "row loja" },
+            itens.map(function(){ 
+             //   return React.createElement(ProductComponent)
+             })
+        )
+    )
+}
+
 function AppComponent(props){
 
-    /**
-<div class="col-sm-4 mb-3">
-        <div class="card ">
-            <div class="card loja__item">
-                <img src="${product.imagem}" class="card-img-top" alt="" />
-                <div class="card-body">
-                    <h5 class="card-title">${product.titulo}</h5>
-                    <small>R$ ${product.preço}</small>
-                    <p class="card-text">${product.descrição}</p>
-                    <button data-index="${index}" class="btn btn-primary btn-add">Adicionar </button>
-                </div>
-            </div>
-        </div>
-    </div>`
-
-
-
-     */
     return (
         React.createElement(React.Fragment,null, 
             React.createElement('div',{className : "col-sm-8" }, 
-                React.createElement('div',{className : "row loja" },
-                    React.createElement('div',{className : "col-sm-4 mb-3" },
-                        React.createElement('div',{className : "card loja__item " },
-                            React.createElement('img',{className : "card-img-top", src: "https://picsum.photos/500/300" }),
-                            React.createElement('div',{className : "card-body" },
-                                React.createElement('h5',{className : "card-title" },'JSRaiz para frameworks'),
-                                React.createElement('small',null,"R$300,00"),
-                                React.createElement('p',{className : "card-text" },'O melhor curso do mundo'),
-                                React.createElement('button',{className : "btn btn-primary" },'Adicionar'),
-
-                            )
-                        )
-                    )
-                )
+            React.createElement(ListaProdutoComponent,{ itens : produtosListas}),
             ),
-
             React.createElement('div',{className : "col-sm-4 mb-3" }, 
                 React.createElement('div',{className : "carrinho" },
                     React.createElement('div',{className : "carrinho__itens" },
@@ -47,18 +91,19 @@ function AppComponent(props){
                                 React.createElement('p',{className : "card-text"}, "Preço unidade: R$ 300,00 | Quantidade 2" ),
                                 React.createElement('p',{className : "card-text"}, "Valor: R$ 600,00" ),
                                 React.createElement('button',{className : "btn btn-danger btn-sm"}, "Remover" ),
-                            ),
-                        ),
+                            )
+                        )
                     ),
                     React.createElement('div',{className : "carrinho__total mt-2 p-3" },
                         React.createElement('h6',null, "Total: ",
                             React.createElement('strong',null, "R$600,00 ")    
-                        )     
+                            )     
+                        )
                     )
                 )
             )
         )
-    )
+    
 }
 
 ReactDOM.render(
