66a17de63512839e7150408e8aaabbc6bf0c10c5
Alex <alex.talha@original.io>
2020-01-10 15:50:43 -0300

    criando evento do click no component

---
+++ props-state/index2.js
@@ -0,0 +1,15 @@
+function Welcome(props) {
+    return "<h1>Hello, {props.name}</h1>";
+  }
+
+
+function App(){
+    
+}
+
+
+
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById('app')
+)
