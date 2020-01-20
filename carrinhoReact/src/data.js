d1a646ced27d4c604caff86bb51c48be967a1914
Alex <Talha>
2020-01-19 23:22:51 -0300

    configurando o webpack

---
+++ carrinhoReact/src/data.js
@@ -0,0 +1,50 @@
+export default  [
+    { 
+        "id" : "0",
+        "imagem" : "https://lorempixel.com/500/300",
+        "titulo" :  "Js Raiz 1",
+        "preço" : 300,
+        "descrição" : "O melhor curso de Javascript 1",
+    
+    },
+    { 
+        "id" : "1",
+        "imagem" : "https://lorempixel.com/500/300",
+        "titulo" :  "Js Raiz 2",
+        "preço" : 301,
+        "descrição" : "O melhor curso de Javascript 2",
+
+    },
+    { 
+        "id" : "2",
+        "imagem" : "https://lorempixel.com/500/300",
+        "titulo" :  "Js Raiz 3",
+        "preço" : 302,
+        "descrição" : "O melhor curso de Javascript 4",
+      
+    },
+    { 
+        "id" : "3",
+        "imagem" : "https://lorempixel.com/500/300",
+        "titulo" :  "Js Raiz 4",
+        "preço" : 303,
+        "descrição" : "O melhor curso de Javascript 4",
+
+    },
+    { 
+        "id" : "4",
+        "imagem" : "https://lorempixel.com/500/300",
+        "titulo" :  "Js Raiz 5",
+        "preço" : 304,
+        "descrição" : "O melhor curso de Javascript 5", 
+    },
+    { 
+        "id" : "5",
+        "imagem" : "https://lorempixel.com/500/300",
+        "titulo" :  "Js Raiz 6",
+        "preço" : 305,
+        "descrição" : "O melhor curso de Javascript 6",
+    },
+    ]
+
+
--- carrinhoReact/src/main.js
+++ carrinhoReact/src/main.js
@@ -1,51 +1,4 @@
-let produtosListas = [
-    { 
-        "id" : "0",
-        "imagem" : "https://lorempixel.com/500/300",
-        "titulo" :  "Js Raiz 1",
-        "preço" : 300,
-        "descrição" : "O melhor curso de Javascript 1",
-    
-    },
-    { 
-        "id" : "1",
-        "imagem" : "https://lorempixel.com/500/300",
-        "titulo" :  "Js Raiz 2",
-        "preço" : 301,
-        "descrição" : "O melhor curso de Javascript 2",
-
-    },
-    { 
-        "id" : "2",
-        "imagem" : "https://lorempixel.com/500/300",
-        "titulo" :  "Js Raiz 3",
-        "preço" : 302,
-        "descrição" : "O melhor curso de Javascript 4",
-      
-    },
-    { 
-        "id" : "3",
-        "imagem" : "https://lorempixel.com/500/300",
-        "titulo" :  "Js Raiz 4",
-        "preço" : 303,
-        "descrição" : "O melhor curso de Javascript 4",
-
-    },
-    { 
-        "id" : "4",
-        "imagem" : "https://lorempixel.com/500/300",
-        "titulo" :  "Js Raiz 5",
-        "preço" : 304,
-        "descrição" : "O melhor curso de Javascript 5", 
-    },
-    { 
-        "id" : "5",
-        "imagem" : "https://lorempixel.com/500/300",
-        "titulo" :  "Js Raiz 6",
-        "preço" : 305,
-        "descrição" : "O melhor curso de Javascript 6",
-    },
-    ]
+import data from './data'
     
 function ProductComponent(props){
 
@@ -190,7 +143,7 @@ function AppComponent(props){
     <React.Fragment>
           <div className="col-sm-8">
               <ListaProdutoComponent>
-                {   produtosListas.map(function(produto,index){ 
+                {   data.map(function(produto,index){ 
                     return  <ProductComponent item={produto} onAddCarrinho={addCarrinho} key={`produto-${index}`} />
                 })}
               </ListaProdutoComponent>
